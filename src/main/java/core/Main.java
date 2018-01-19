8970ee47804f2ea3a4b5390ce8e7d2d24b8b88a8
SERIAL\tchesse <tchesse@serial.ch>
2018-01-19 15:06:51 +0100

    Initial commit

---
+++ src/main/java/core/Main.java
@@ -0,0 +1,26 @@
+package core;
+
+	/*
+	 * Door Two
+	 * 
+	 * Make a method that takes both a name and a surname in parameter,
+	 * then returns a concatenation of both capitalized, followed by 
+	 * today's date minus a week with the following format: YYYYddMM.
+	 * (If today's date is 2018-03-03, then it should be 20182402... or 20182502 ?)
+	 * 
+	 * ex. : "GUILLAUMEDELAFOSSE_20180101"
+	 * 
+	 * Print it into the console.
+	 * Zip the project and set your first name as a password,
+	 * name the zip folder with your last name, and store it in the "result" folder.
+	 * 
+	 */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
